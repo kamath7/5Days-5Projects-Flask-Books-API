7fd882526aff259b450a0c16f7de9af6c834ab33
Adithya Kamath <adithyakamath96@gmail.com>
2020-10-02 04:29:43 +0530

    Create and Read routes done

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 
-@app.route('/')
+myclient = pymongo.MongoClient(os.getenv("MONGO_DB_URI"))
+mydb = myclient["lalle"]
+mycol = mydb["books"]
+
+
+@app.route("/")
 def hello():
-    return 'Hello Kams!'
+    return "Hello Kams!"
 
-@app.route('/movies')
-def movies_fav():
-    return 'Favorite movies include Fight Club, Usual Suspects, Rangitaranga'
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/books")
+def get_book():
+    books = []
+    for values in mycol.find():
+        values["_id"] = str(values["_id"])
+        books.append(values)
+    return jsonify(lalle=books)
+
+
+@app.route("/books/insert", methods=["POST"])
+def insert_book():
+    if request.method == "POST":
+        lalle = request.json
+        book_name = lalle["name"]
+        book_category = lalle["category"]
+        book_read = lalle["read"]
+        book_to_insert = {
+            "name": book_name,
+            "category": book_category,
+            "read": book_read,
+        }
+        x = mycol.insert_one(book_to_insert)
+        return "Successfully added -> {}".format(
+            book_to_insert,
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
